d574744ee990b49ce431806b141059f1d3334bc2
Dan Sensecall <dan@sensecall.co.uk>
2018-01-09 14:48:59 +0000

    first

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,33 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+	window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
 $(document).ready(function () {
-  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
-  // with role="button" when the space key is pressed.
-  GOVUK.shimLinksWithButtonRole.init()
+	// Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+	// with role="button" when the space key is pressed.
+	GOVUK.shimLinksWithButtonRole.init()
 
-  // Details/summary polyfill from frontend toolkit
-  GOVUK.details.init()
+	// Details/summary polyfill from frontend toolkit
+	GOVUK.details.init()
 
-  // Show and hide toggled content
-  // Where .multiple-choice uses the data-target attribute
-  // to toggle hidden content
-  var showHideContent = new GOVUK.ShowHideContent()
-  showHideContent.init()
+	// Show and hide toggled content
+	// Where .multiple-choice uses the data-target attribute
+	// to toggle hidden content
+	var showHideContent = new GOVUK.ShowHideContent()
+	showHideContent.init()
+	
+	
+	$('#upload').on('change',function (e) {
+		$('#upload-select').addClass('hidden');
+		$('#uploading-progress').removeClass('hidden');
+		
+		var fileName = e.target.files[0].name;
+
+		$('#file-name').text("Uploading " + fileName );
+		setTimeout(function () {
+			$('#upload-select').submit()
+		}, 3000);
+	})
 })
--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,25 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Add your routes here - above the module.exports line
+// Route index page
+router.get('/submit-ilr', function (req, res) {
+  res.render('submit-ilr')
+})
+
+
+// Branching
+router.get('/examples/over-18', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  var over18 = req.query.file-upload
+
+  if (over18 === 'false') {
+    // Redirect to the relevant page
+    res.redirect('/docs/examples/under-18')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('examples/over-18')
+  }
+})
+
 
 module.exports = router
