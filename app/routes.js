0957e7090d99bd5b1026c971f930ed94dfe71e6f
Dan Sensecall <dan@sensecall.co.uk>
2018-01-16 10:17:49 +0000

    config urls

--- app/routes.js
+++ app/routes.js
@@ -3,19 +3,21 @@ const router = express.Router()
 
 // Route index page
 router.get('/', function (req, res) {
-  res.render('hub-v1')
+	res.render('hub')
 })
 
+//end test
+
 router.get('/submit-ilr', function (req, res) {
-  res.render('submit-ilr')
+	res.render('submit-ilr')
 })
 
 router.get('/submission-complete', function (req, res) {
-  res.render('submission-complete-v1')
+	res.render('submission-complete-v1')
 })
 
 router.get('/prototype-admin/config', function (req, res) {
-  res.render('config')
+	res.render('config')
 })
 
 module.exports = router
